b17bf5d0650b5af581ac5934001d7fc59295e591
Bill Bushey <wbushey@gmail.com>
2014-04-03 11:53:33 -0500

    Initial commit

---
+++ README.txt
@@ -0,0 +1,35 @@
+==============================
+``edem.site.about``
+==============================
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Customized version of the site home about box for E-Democracy
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:Author: `Bill Bushey`_
+:Contact: Bill Bushey <bill.bushey@e-democracy.org>
+:Date: 2014-04-03
+:Organization: `E-Democracy.org`_
+:Copyright: This document is licensed under a
+  `Creative Commons Attribution-Share Alike 3.0 License`_
+  by `E-Democracy.org`_.
+
+Introduction
+===========
+
+This egg customizes the about box on the homepage of E-Democracy forums
+provided by `gs.site.about`_. Specifically, it makes the about box expand by
+default.
+
+Resources
+=========
+
+- Code repository: https://github.com/e-democracy/edem.site.about
+- Questions and comments to http://groupserver.org/groups/development
+- Report bugs at https://redmine.iopen.net/projects/edem
+
+.. _GroupServer: http://groupserver.org/
+.. _E-Democracy.org: http://e-democracy.org/
+.. _Bill Bushey: http://groupserver.org/p/wbushey
+.. _Creative Commons Attribution-Share Alike 3.0 License:
+   http://creativecommons.org/licenses/by-sa/3.0/
+.. _gs.site.about: https://source.iopen.net/groupserver/gs.site.about/
---
+++ setup.py
@@ -0,0 +1,46 @@
+# -*- coding=utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+import os
+from setuptools import setup, find_packages
+from version import get_version
+
+version = get_version()
+
+setup(
+    name='edem.site.about',
+    version=version,
+    description="Customized version of the site home about box for "
+    "E-Democracy",
+    long_description=open("README.txt").read() + "\n" +
+    open(os.path.join("docs", "HISTORY.txt")).read(),
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers for values
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later "
+        "(GPLv3+)",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux"
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='',
+    author='Bill Bushey',
+    author_email='bill.bushey@e-democracy.org',
+    url='http://www.e-democracy.org/',
+    license='GPL 3',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['edem', 'edem.group', 'edem.group.messages'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'edem.skin',
+        'gs.site.home',
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,)
